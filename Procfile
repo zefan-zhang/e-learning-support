8972a83ac99e135f3f45284937405db431a207c5
zefan-zhang <kd.zefan@gmail.com>
2020-07-09 16:15:57 -0400

    added hello world for testing

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -Dserver.port=$PORT $JAVA_OPTS -jar target/e-learning-support-0.0.1-SNAPSHOT.war
+web: java -Dserver.port=$PORT $JAVA_OPTS -jar target/e-learning-support-0.0.1-SNAPSHOT.jar
---
+++ src/main/java/zefan/project/elearningsupport/HelloRepository.java
@@ -0,0 +1,6 @@
+package zefan.project.elearningsupport;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface HelloRepository
+        extends CrudRepository<Hello, Integer> {}
