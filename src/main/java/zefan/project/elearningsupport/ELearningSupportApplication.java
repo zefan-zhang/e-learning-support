f944c6b7cba3ce0ebeeb78ea826d6202ce66d14e
zefan-zhang <kd.zefan@gmail.com>
2020-07-09 13:30:34 -0400

    first commit

---
+++ src/main/java/zefan/project/elearningsupport/ELearningSupportApplication.java
@@ -0,0 +1,13 @@
+package zefan.project.elearningsupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELearningSupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELearningSupportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zefan/project/elearningsupport/ELearningSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package zefan.project.elearningsupport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearningSupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
